9000338b6e5d17df77548338b70b894464bf1007
ZayatsK <Zorex2008@yandex.ru>
2022-01-05 23:42:54 +0700

    Домашка 3

--- src/main/java/HomeWork/HomeWork_3.java
+++ src/main/java/HomeWork/HomeWork_3.java
@@ -60,6 +60,7 @@ arr [0][0] = 1;
             arr [3][3] = 1;
             arr [3][0] = 1;
             arr [2][1] = 1;
+
             arr [0][3] = 1;
             arr [1][2] = 1;
             for (int i = 0; i < arr.length; i++) {
